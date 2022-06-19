7c4e9a1b591736cd4fa351e2740c9432e5a4ed1c
Luxx <lakshrajme@gmail.com>
2022-06-19 10:55:22 +0530

    Created Search Input and Fetch Results

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>This is Home.js</MainPageLayout>;
+  // eslint-disable-next-line
+  const [input, setinput] = useState('');
+
+  const onInputChange = ev => {
+    setinput(ev.target.value);
+  };
+
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        onKeyDown={onKeyDown}
+        type="text"
+        onChange={onInputChange}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
