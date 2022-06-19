15cd3b1efe34da9f9f400865492a9eb84a306f3f
Luxx <lakshrajme@gmail.com>
2022-06-19 08:49:54 +0530

    added React Routers #1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is Home PAGE
+      </Route>
+
+      <Route exact path="/starred">
+        This is Starred
+      </Route>
+
+      <Route>Eorror 404 - Page not Fount</Route>
+    </Switch>
+  );
 }
 export default App;
